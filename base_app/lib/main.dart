e1756f489dc4fce436b3a6f1353e5ec0c198af7f
Allan Oliveira <allanoliveira@outlook.com>
2023-03-12 15:25:47 -0300

    Initial commit

---
+++ base_app/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:micro_app_publish_vacancy/app/publish_vacancy_resolver.dart';
+import 'package:micro_core/app/base_app.dart';
+import 'package:micro_core/app/micro_app.dart';
+import 'package:micro_core/app/micro_core_utils.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget with BaseApp {
+  MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    super.registerRoutes();
+
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      navigatorKey: navigatorKey,
+      onGenerateRoute: super.generateRoute,
+      initialRoute: '/publish_vacancy',
+    );
+  }
+
+  @override
+  Map<String, WidgetBuilderArgs> get baseRoutes => {};
+
+  @override
+  List<MicroApp> get microApps => [PublishVacancyResolver()];
+}
